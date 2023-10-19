d1ffa3675b83fe7558bfb0cbf6067a84b6e39789
Alex Bowes <alex.bowes@blueambertech.co.uk>
2023-10-19 11:49:36 +0100

    Return interface don't populate

--- db.go
+++ db.go
@@ -5,8 +5,8 @@ import (
 )
 
 type NoSQLClient interface {
-	Read(ctx context.Context, collection, id string, outObj interface{}) error
+	Read(ctx context.Context, collection, id string) (interface{}, error)
 	Insert(ctx context.Context, collection string, data interface{}) (string, error)
 	InsertWithID(ctx context.Context, collection, id string, data interface{}) error
-	Where(ctx context.Context, collection, key, value string, outObjs []map[string]interface{}) error
+	Where(ctx context.Context, collection, key, value string) ([]map[string]interface{}, error)
 }
