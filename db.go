8999312f9249df5b68c9d7826080a88f77152976
Alex Bowes <alex.bowes@blueambertech.co.uk>
2023-10-20 12:26:33 +0100

    Change return type of Read

--- db.go
+++ db.go
@@ -5,7 +5,7 @@ import (
 )
 
 type NoSQLClient interface {
-	Read(ctx context.Context, collection, id string) (interface{}, error)
+	Read(ctx context.Context, collection, id string) (map[string]interface{}, error)
 	Insert(ctx context.Context, collection string, data interface{}) (string, error)
 	InsertWithID(ctx context.Context, collection, id string, data interface{}) error
 	Where(ctx context.Context, collection, key, value string) ([]map[string]interface{}, error)
