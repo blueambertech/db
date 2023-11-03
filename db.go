2b1d36dfbca0e7462a3d13565a818f987ae10537
Alex Bowes <alex.bowes@blueambertech.co.uk>
2023-11-03 10:01:21 +0000

    Add operator to Where

--- db.go
+++ db.go
@@ -8,6 +8,6 @@ type NoSQLClient interface {
 	Read(ctx context.Context, collection, id string) (map[string]interface{}, error)
 	Insert(ctx context.Context, collection string, data interface{}) (string, error)
 	InsertWithID(ctx context.Context, collection, id string, data interface{}) error
-	Where(ctx context.Context, collection, key, value string) ([]map[string]interface{}, error)
+	Where(ctx context.Context, collection, key, operator, value string) ([]map[string]interface{}, error)
 	Exists(ctx context.Context, collection, id string) (bool, error)
 }
